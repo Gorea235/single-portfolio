de56e7fe1e55b721b736bdab25110a366ded1e7d
Luke Davies <lkdvs3@gmail.com>
2018-04-05 00:24:25 +0100

    Added remainding endpoints w/ no logic and cleaned SQL

--- node/api/galleries.ts
+++ node/api/galleries.ts
@@ -3,47 +3,84 @@ import { ApiRoute } from './base';
 import { Connection } from 'mysql';
 
 export class Galleries implements ApiRoute {
+    private sqlPrimerRe = /'/g;
+
+    // Helper SQL strings
+
+    // gallery exists
+    private sqlGalleryExists = this.sqlPrimer(`
+SELECT EXISTS(SELECT 1 FROM 'Galleries' WHERE 'Id' = ? LIMIT 1)
+`);
+
+    // GET SQL strings
+
     // gallery listing
-    private sqlGalleryList = `
-SELECT \`id\`, \`name\`, \`desc\`, \`dateAdded\`, \`dateUpdated\`
-FROM \`Galleries\`
-`;
+    private sqlGalleryList = this.sqlPrimer(`
+SELECT 'id', 'name', 'desc', 'dateAdded', 'dateUpdated'
+FROM 'Galleries'
+`);
     // select a single gallery based on id
-    private sqlGallerySelect = this.sqlGalleryList + `
-WHERE \`id\` = ?
-`;
+    private sqlGallerySelect = this.sqlPrimer(this.sqlGalleryList + `
+WHERE 'id' = ?
+`);
     // list images in a gallery
-    private sqlGalleryImageList = `
-SELECT \`GalleryImages\`.\`id\`, \`GalleryImages\`.\`galleryId\`,
-    \`GalleryImages\`.\`desc\`, \`GalleryImages\`.\`dateTaken\`,
-    \`GalleryImages\`.\`imageData\`,
-    \`ImageKind\`.\`id\` as \`imageKindId\`, \`ImageKind\`.\`name\` as \`imageKindName\`,
-    \`ImageKind\`.\`desc\` as \`imageKindDesc\`
-FROM \`GalleryImages\`
-INNER JOIN \`ImageKind\` ON \`GalleryImages\`.\`imageKindId\` = \`ImageKind\`.\`id\`
-WHERE \`GalleryImages\`.\`galleryId\` = ?
-`;
-    private sqlGalleryImageSelect = this.sqlGalleryImageList + `
-AND \`GalleryImages\`.\`id\` = ?
-`;
+    private sqlGalleryImageList = this.sqlPrimer(`
+SELECT 'GalleryImages'.'id', 'GalleryImages'.'galleryId',
+    'GalleryImages'.'desc', 'GalleryImages'.'dateTaken',
+    'GalleryImages'.'imageData',
+    'ImageKind'.'id' as 'imageKindId', 'ImageKind'.'name' as 'imageKindName',
+    'ImageKind'.'desc' as 'imageKindDesc'
+FROM 'GalleryImages'
+INNER JOIN 'ImageKind' ON 'GalleryImages'.'imageKindId' = 'ImageKind'.'id'
+WHERE 'GalleryImages'.'galleryId' = ?
+`);
+    private sqlGalleryImageSelect = this.sqlPrimer(this.sqlGalleryImageList + `
+AND 'GalleryImages'.'id' = ?
+`);
     // list categories in an image
-    private sqlGalleryImageCategoryList = `
-SELECT \`Categories\`.\`id\`, \`Categories\`.\`name\`, \`Categories\`.\`desc\`
-FROM \`Categories\`
-INNER JOIN \`GalleryImageCategories\` ON \`GalleryImageCategories\`.\`id\` = \`Categories\`.\`id\`
-WHERE \`GalleryImageCategories\`.\`id\` = ?
-`;
+    private sqlGalleryImageCategoryList = this.sqlPrimer(`
+SELECT 'Categories'.'id', 'Categories'.'name', 'Categories'.'desc'
+FROM 'Categories'
+INNER JOIN 'GalleryImageCategories' ON 'GalleryImageCategories'.'id' = 'Categories'.'id'
+WHERE 'GalleryImageCategories'.'id' = ?
+`);
+
+    // POST SQL strings
+
+    // PATCH SQL strings
+
+    // DELETE SQL strings
+
 
     constructor(
         private dbConn: Connection
     ) { }
 
     mountRoutes(router: Router) {
+        // GETs
         router.get('/galleries', (req, res) => this.listGalleries(req, res));
         router.get('/galleries/:galleryId', (req, res) => this.getGallery(req, res));
         router.get('/galleries/:galleryId/images', (req, res) => this.listImages(req, res));
         router.get('/galleries/:galleryId/images/:imageId', (req, res) => this.getImage(req, res));
+        // POSTs
+        router.post('/galleries', (req, res) => this.createGallery(req, res));
+        router.post('/galleries/:galleryId/images', (req, res) => this.createImage(req, res));
+        // PATCHes
+        router.patch('/galleries/:galleryId', (req, res) => this.updateGallery(req, res));
+        router.patch('/galleries/:galleryId/images/:imageId', (req, res) => this.updateImage(req, res));
+        // DELETEs
+        router.delete('/galleries/:galleryId', (req, res) => this.deleteGallery(req, res));
+        router.delete('/galleries/:galleryId/images/:imageId', (req, res) => this.deleteImage(req, res));
     }
+
+    private sqlPrimer(sql: string): string {
+        // in order to stop excessive \` in the SQL strings,
+        // we use ' and replace with ` later
+        // this will only affect boot time, and not by much
+        return sql.replace(this.sqlPrimerRe, '`');
+    }
+
+    // GET (read) endpoints
 
     private listGalleries(req: Request, res: Response): void {
         this.dbConn.query(this.sqlGalleryList,
@@ -125,5 +162,35 @@ WHERE \`GalleryImageCategories\`.\`id\` = ?
                         res.json(img);
                     });
             });
+    }
+
+    // POST (create) endpoints
+
+    private createGallery(req: Request, res: Response): void {
+
+    }
+
+    private createImage(req: Request, res: Response): void {
+
+    }
+
+    // PATCH (update) endpoints
+
+    private updateGallery(req: Request, res: Response): void {
+
+    }
+
+    private updateImage(req: Request, res: Response): void {
+
+    }
+
+    // DELETE (delete) endpoints
+
+    private deleteGallery(req: Request, res: Response): void {
+
+    }
+
+    private deleteImage(req: Request, res: Response): void {
+
     }
 }
