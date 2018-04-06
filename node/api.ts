292974d8797b030ba3f3f2a46d1ab60a08c86458
Luke Davies <lkdvs3@gmail.com>
2018-04-06 21:34:08 +0100

    Added search api endpoint

--- node/api.ts
+++ node/api.ts
@@ -3,10 +3,11 @@ import { Index, ApiRoute } from './api/base';
 import { Connection } from 'mysql';
 
 // API imports
+import { Auther } from './auther';
 import { Auth } from './api/auth';
 import { Galleries } from './api/galleries';
 import { Config } from './api/config';
-import { Auther } from './auther';
+import { Search } from './api/search';
 
 export class ApiRouter {
     private apiRoutes: ApiRoute[];
@@ -25,6 +26,7 @@ export class ApiRouter {
         this.apiRoutes.push(new Auth(this.auther, this.dbConn));
         this.apiRoutes.push(new Galleries(this.dbConn));
         this.apiRoutes.push(new Config(this.auther, this.dbConn));
+        this.apiRoutes.push(new Search(this.dbConn));
 
         // mount routes
         this.apiRoutes.forEach(apiRoute => apiRoute.mountRoutes(this.router));
